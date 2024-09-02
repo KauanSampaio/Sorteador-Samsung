28137e38ca7c3bbf94129e1dd592d6c657037324
KauanSampaio <kauansampaio53@gmail.com>
2024-09-02 03:13:11 -0300

    finished logic

--- script.js
+++ script.js
@@ -6,4 +6,68 @@ button.addEventListener("click", () => {
 
     // Removendo todos os elementos vazios que estejam presentes no array
     nameList = nameList.filter((element) => element != '');
+
+    // Pegando a quantidade de times e o elemento onde será criado a tabela
+    let numTeams = document.querySelector("#numTeams").value;
+    // Tratando a entrada de dados no input de quantidade de times
+    if(!numTeams || numTeams <=0 || numTeams > nameList.length) {
+        alert("Quantidade de times inválida! Tente novamente.");
+        return;
+    }
+
+    let divTable = document.querySelector("#teamsTable");
+    
+    // Criando a tabela
+    let newTable = document.createElement("table");
+    let tableHeader = document.createElement("thead");
+    let tableBody = document.createElement("tbody");
+
+    // Populando o cabeçalho da tabela
+    for(let i = 1; i<=numTeams; i++) {
+        let headerRow = document.createElement("th");
+        headerRow.style.border = "solid";
+        headerRow.appendChild(document.createTextNode(`Time ${i}`));
+        tableHeader.appendChild(headerRow);
+    }
+
+    // Sorteando os times e populando o corpo da tabela
+    let realNumberOfNames = nameList.length;
+    for(let i = 0; i < parseInt((realNumberOfNames / numTeams)); i++) {
+        let bodyRow = document.createElement("tr");
+        for(let j = 0; j < numTeams; j++) {
+            let numSorted = parseInt(Math.random() * (nameList.length - 0) + 0);
+            let bodyRowContent = document.createElement("td");
+            bodyRowContent.appendChild(document.createTextNode(nameList[numSorted]));
+            bodyRow.appendChild(bodyRowContent);
+            nameList.splice(numSorted, 1);
+        }
+        tableBody.appendChild(bodyRow);
+    }
+
+    // Verificando se sobraram nomes sem serem sorteados
+    if(nameList.length != 0){
+        // Caso tenham sobrado, sorteia eles entre os times até não restarem nenhum
+        realNumberOfNames = nameList.length;
+        let bodyRow = document.createElement("tr");
+        for(let i = 0; i<realNumberOfNames; i++) {
+            let numSorted = parseInt(Math.random() * (nameList.length - 0) + 0);
+            let bodyRowContent = document.createElement("td");
+            bodyRowContent.appendChild(document.createTextNode(nameList[numSorted]));
+            bodyRow.appendChild(bodyRowContent);
+            nameList.splice(numSorted, 1);
+        }
+        tableBody.appendChild(bodyRow);
+    }
+    
+    // Populando a tabela com todos os dados
+    newTable.appendChild(tableHeader);
+    newTable.appendChild(tableBody);
+
+    // Gerando a tabela na tela
+    divTable.appendChild(newTable);
+
+    // Ajustando a borda da tabela
+    newTable.style.border = "solid";
+
+    // document.querySelector("#sortForm").innerHTML = "";
 });
